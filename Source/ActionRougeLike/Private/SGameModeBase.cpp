44aa1be367bd6062436aacbc31752903dc26be55
dloe <dylanloe@gmail.com>
2023-03-15 18:33:30 -0700

    Made base game mode for spawning AI
    - small fix to AI
    - main structure of game mode

---
+++ Source/ActionRougeLike/Private/SGameModeBase.cpp
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SGameModeBase.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "EnvironmentQuery/EnvQueryManager.h"
+#include "EnvironmentQuery/EnvQueryInstanceBlueprintWrapper.h"
+#include <ActionRougeLike/Public/AI/SAICharacter.h>
+#include <ActionRougeLike/Public/SAttributeComponent.h>
+#include "EngineUtils.h"
+//dont think the eenvqueryrunmode enum is needed to include header
+
+
+ASGameModeBase::ASGameModeBase()
+{
+	SpawnTimerInterval = 2.0f;
+
+}
+
+void ASGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+
+	GetWorldTimerManager().SetTimer(TimerHandle_SpawnBots, this, &ASGameModeBase::SpawnBotTimerElapsed, SpawnTimerInterval, true);
+}
+
+
+void ASGameModeBase::SpawnBotTimerElapsed()
+{
+	//eqs query for spawning bots
+	//using old style of enums (dont really use this much anymore)
+	//random best 5%
+	//never really use the last param
+	UEnvQueryInstanceBlueprintWrapper* QueryInstance = UEnvQueryManager::RunEQSQuery(this, SpawnBotQuery, this, EEnvQueryRunMode::RandomBest5Pct, nullptr);
+
+	QueryInstance->GetOnQueryFinishedEvent().AddDynamic(this, &ASGameModeBase::OnQueryCompleted);
+
+}
+
+void ASGameModeBase::OnQueryCompleted(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus)
+{
+	//not using ensure because it would be triggering every 2 frames and having this check is instantatious (more useful this way)
+	if (QueryStatus != EEnvQueryStatus::Success)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Spawn Bot EQS Query Failed!"));
+		return;
+	}
+
+	int32 NrOfAliveBots = 0;
+	//
+	for (TActorIterator<ASAICharacter> It(GetWorld()); It; ++It)
+	{
+		ASAICharacter* Bot = *It;
+
+		//will eventually as is alive function
+		//if (Bot->Alive())
+		USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(Bot->GetComponentByClass(USAttributeComponent::StaticClass()));
+		if(AttributeComp && AttributeComp->IsAlive())
+		{
+			NrOfAliveBots++;
+		}
+	}
+
+	float MaxBotCount = 10.0f;
+	if (DifficultyCurve)
+	{
+		MaxBotCount = DifficultyCurve->GetFloatValue(GetWorld()->TimeSeconds);
+	}
+
+	if (NrOfAliveBots >= MaxBotCount)
+	{
+		return;
+	}
+
+	
+	//give us the result and location for where we want to spawn
+	//status tells us if it fails, etc
+	TArray<FVector> Locations = QueryInstance->GetResultsAsLocations();
+	
+	//check our locations
+	if (Locations.IsValidIndex(0))
+	{
+
+		GetWorld()->SpawnActor<AActor>(MinionClass, Locations[0], FRotator::ZeroRotator);
+	}
+}
